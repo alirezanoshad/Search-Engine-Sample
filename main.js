22ba560e140727840fbb07bedef239cf1d130146
Alireza <iranexplosm@gmail.com>
2024-08-04 01:30:37 +0430

    -few js commnets added for better user perspective
    -few js commnets added in order to be more clear on js coding lines..

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+// Following line got basic text content of website
 const textToSerach = "ak-47 muslim allahuAkbar boombastic going to moon";
+// Following line made it user to write down the text to search(search engine)
 const targetString =  prompt("Search:");
-
+//ifs and elses of if following searched phrase is availabe or not and how to respond to it
 if(textToSerach.includes(targetString)){
     console.log(`${targetString} Founded!`);
 } else {
